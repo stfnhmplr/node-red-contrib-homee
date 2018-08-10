5cdfb89abff4c04254dc34bfea663ce95a85f37f
stfnhmplr <stfnhmplr@gmail.com>
2018-08-10 09:19:09 +0200

    fixed device-name and node states

--- homee.js
+++ homee.js
@@ -4,20 +4,41 @@ module.exports = function(RED) {
     function HomeeNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
+        node.status({fill: 'red', shape: 'dot', text: 'disconnected'});
 
-        var homee = new Homee(config.host, this.credentials.user, this.credentials.pass);
-
-        homee.connect().then(function () {
-            node.log('connected to homee');
-            node.status({fill: 'green', shape: 'dot', text: 'connected'});
-        }).catch(function (err) {
-            node.log.error(err);
-            node.status({fill: 'red', shape: 'dot', text: 'error'});
+        var homee = new Homee(config.host, this.credentials.user, this.credentials.pass, {
+            device: 'Node-RED',
+            reconnect: true,
+            reconnectInterval: 5000,
+            maxRetries: Infinity
         });
+
+        homee.on('connected', function () {
+            node.status({fill: 'green', shape: 'dot', text: 'connected'});
+        });
+
+        homee.on('disconnected', function () {
+            node.status({fill: 'red', shape: 'dot', text: 'disconnected'});
+        })
+
+        homee.on('reconnect', function () {
+            node.status({fill: 'yellow', shape: 'dot', text: 'reconnecting...'})
+        })
 
         homee.on('message', function (message) {
             node.send({ payload: message });
         })
+
+        homee.on('error', function (err) {
+            node.log(err);
+        })
+
+        homee.connect().then(function () {
+            node.log('connected to homee');
+        }).catch(function (err) {
+            node.log.error(err);
+            node.status({fill: 'red', shape: 'dot', text: 'error'});
+        });
 
         this.on('input', function(msg) {
             homee.send(msg.payload);
@@ -25,7 +46,6 @@ module.exports = function(RED) {
 
         this.on('close', function () {
             homee.disconnect();
-            node.status({fill: 'red', shape: 'dot', text: 'not connected'});
             node.log('homee: connection closed')
         });
     }
