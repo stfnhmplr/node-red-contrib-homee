a3c103f55f82e5f7350da49e86bc2221f8ba1a84
stfnhmplr <stfnhmplr@gmail.com>
2019-11-05 21:45:16 +0100

    added test

---
+++ test/homee_spec.js
@@ -0,0 +1,20 @@
+const should = require('should');
+const helper = require('node-red-node-test-helper');
+const homeeNode = require('../nodes/homee.js');
+
+helper.init(require.resolve('node-red'));
+
+describe('homee Node', () => {
+  before((done) => helper.startServer(done));
+  after((done) => helper.stopServer(done));
+  afterEach(() => helper.unload());
+
+  it('should be loaded', (done) => {
+    const flow = [{ id: 'n1', type: 'homee', name: 'homee' }];
+    helper.load(homeeNode, flow, () => {
+      const n1 = helper.getNode('n1');
+      n1.should.have.property('name', 'homee');
+      done();
+    });
+  });
+});
